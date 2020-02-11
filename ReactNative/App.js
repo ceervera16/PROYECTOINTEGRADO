85049251216bd89e2b8e254b6fdae515cd88fb68
Guillermo <guillermo_m_96@hotmail.com>
2020-02-11 12:03:47 +0100

    Añadidas pantallas de la aplicación y navegación por tabs

--- ReactNative/App.js
+++ ReactNative/App.js
@@ -1,114 +1,101 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { createContext } from 'react';
+import { Text } from 'react-native';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import 'react-native-gesture-handler';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+import Motor1Screen from './screens/Motor1Screen';
+import Motor2Screen from './screens/Motor2Screen';
+import Motor3Screen from './screens/Motor3Screen';
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createAppContainer } from 'react-navigation';
 
-export default App;
+export const TabNavigator = createBottomTabNavigator(
+  {
+    "G1": Motor1Screen,
+    "G2": Motor2Screen,
+    "G3": Motor3Screen,
+  },
+  {
+    tabBarOptions: {
+      activeBackgroundColor: "rgba(16,110,242,1)",
+      shifting: true,
+      style: {
+        backgroundColor: '#f7f7f7',
+        height: 70,
+      },
+    },
+    defaultNavigationOptions: ({ navigation, screenProps }) => ({
+      tabBarLabel: ({ focused }) => (
+        <Text style={{ fontSize: focused ? 20 : 15, textAlign: 'center', color: focused ? 'white' : 'rgba(16,110,242,1)', marginBottom: 5, marginTop: -5 }}>
+          {(screenProps.english ? "Group " : "Grupo ") + navigation.state.routeName.substring(1, 2)}
+        </Text>
+      ),
+      tabBarIcon: ({ focused }) => (
+        <Icon
+          name="engine"
+          size={focused ? 35 : 30}
+          color={
+            navigation.state.routeName.substring(1, 2) == 1
+              ? (screenProps.variables.G1Marcha ? "#7BE756" : "#F44138")
+              : navigation.state.routeName.substring(1, 2) == 2
+                ? (screenProps.variables.G2Marcha ? "#7BE756" : "#F44138")
+                : (screenProps.variables.G3Marcha ? "#7BE756" : "#F44138")
+          }
+        />
+      ),
+    }),
+  },
+);
+
+const AppContainer = createAppContainer(TabNavigator);
+
+export const VariablesContext = createContext(null)
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      datos: [],
+      english: false,
+    }
+  }
+
+  onModLang = () => {
+    this.setState({ english: !this.state.english });
+  }
+
+  getDatos = async () => {
+    try {
+      const response = await fetch(`http://10.0.2.2:44325/api/Variables`);
+      const json = response.ok ? await response.json() : console.log("Error");
+      this.setState({ datos: json });
+      console.log(this.state.datos);
+    }
+    catch (error) {
+      console.log("Error: " + error);
+    }
+  }
+
+  componentDidMount = () => {
+    this.getDatos();
+
+    setInterval(() => {
+      this.getDatos();
+    }, 10000);
+  }
+
+  render() {
+    return (
+      <AppContainer
+        screenProps={{
+          variables: this.state.datos,
+          english: this.state.english,
+          onModLang: this.onModLang,
+        }}
+      />
+    );
+  }
+}
---
+++ ReactNative/screens/Motor3Screen.js
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react';
+import {
+  StyleSheet,
+  View,
+  TouchableHighlight,
+  Image,
+} from 'react-native';
+
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+export default class Motor3Screen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        
+      </View>
+    )
+  }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "lightgrey",
+    flex: 10
+  },
+});
